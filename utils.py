02a3eb929a3ea6045176b87d5e5083f5ac0f53ec
Ivan <grecigor11@gmail.com>
2021-10-01 01:37:40 +0300

    pep-8

--- utils.py
+++ utils.py
@@ -5,10 +5,10 @@ import requests
 from fake_useragent import UserAgent
 import pandas as pd
 
-def get_json_from_history_data(name_of_company:str):
 
+def get_json_from_history_data(name_of_company: str):
     ua = UserAgent()
-    headers = {'user-agent':ua['google chrome']}
+    headers = {'user-agent': ua['google chrome']}
     period2 = int(time.mktime(datetime.datetime.now().timetuple()))
 
     csv_download = f'https://query1.finance.yahoo.com/v7/finance/download/{name_of_company}?period1=1&period2={period2}&interval=1d&events=history&includeAdjustedClose=true'
@@ -18,17 +18,17 @@ def get_json_from_history_data(name_of_company:str):
     # return df.to_json(orient='records', lines=True)
 
     # Way with requests
-    r = requests.get(csv_download,headers=headers)
-    raw_data  = r.text.split(',')
+    r = requests.get(csv_download, headers=headers)
+    raw_data = r.text.split(',')
     data = []
-    for i in range(6,len(raw_data)-6,6):
+    for i in range(6, len(raw_data) - 6, 6):
         data.append({
             raw_data[0]: raw_data[i],
-            raw_data[1]: raw_data[i+1],
-            raw_data[2]: raw_data[i+2],
-            raw_data[3]: raw_data[i+3],
-            raw_data[4]: raw_data[i+4],
-            raw_data[5]: raw_data[i+5],
-            raw_data[6].split()[0]:raw_data[6].split()[1]
+            raw_data[1]: raw_data[i + 1],
+            raw_data[2]: raw_data[i + 2],
+            raw_data[3]: raw_data[i + 3],
+            raw_data[4]: raw_data[i + 4],
+            raw_data[5]: raw_data[i + 5],
+            raw_data[6].split()[0]: raw_data[6].split()[1]
         })
     return data
